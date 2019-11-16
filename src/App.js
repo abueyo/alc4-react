b8038d3a0e6b982f8972a71e7c879f5411c36d82
Abu <abueyoabu28@gmail.com>
2019-11-17 00:38:34 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>ALC 4.0 Cloud Challenge I</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+          Challenge:&nbsp;
+          <a 
+          className="App-status"
+          href="#"
+          target=" blank"
+          rel="noopener noreferrer"
+          >
+          Accepted
+          </a>
+          </p>
         <a
           className="App-link"
           href="https://reactjs.org"
